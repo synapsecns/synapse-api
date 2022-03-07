5f60eee730d92d7a044217c50aa6ac5b117c4575
bholagabbar <dev.shreyans96@gmail.com>
2022-03-06 20:02:52 -0500

    Move to npm SDK

--- sdk/bridge/slippages.d.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber } from "@ethersproject/bignumber";
-export declare namespace Slippages {
-    type Slippage = string;
-    const One: Slippage, OneTenth: Slippage, TwoTenth: Slippage, Quarter: Slippage;
-    function _applySlippage(inputValue: BigNumber, slippageSelected: string | Slippage, add?: boolean): BigNumber;
-    function addSlippage(inputValue: BigNumber, slippageSelected: string | Slippage): BigNumber;
-    function subtractSlippage(inputValue: BigNumber, slippageSelected: string | Slippage): BigNumber;
-    function formatSlippageToString(slippageSelected: string | Slippage): string;
-}
