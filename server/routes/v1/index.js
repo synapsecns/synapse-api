faec900eb4c0a39c089e1e22f840c1cfcdf8c2c8
bholagabbar <dev.shreyans96@gmail.com>
2022-03-24 16:51:31 -0400

    Add more error and response docs

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -26,20 +26,29 @@ import {BigNumber} from "ethers";
  *
  * @apiQuery {Number|String} chain Chain id passed as a decimal or hex number
  *
+ * @apiExample {curl} Example usage:
+ * curl --request GET 'https://syn-api-x.herokuapp.com/v1/get_bridgable_tokens?chain=ARBITRUM'
+ *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
- * [
+ *     [
+ *         {
+ *             "name": "Binance USD",
+ *             "symbol": "BUSD",
+ *             "decimals": 18,
+ *             "addresses": {
+ *                 "56": "0xe9e7cea3dedca5984780bafc599bd69add087d56"
+ *             },
+ *             "swapType": "USD"
+ *         }
+ *         ...
+ *     ]
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
  *     {
- *         "name": "Binance USD",
- *         "symbol": "BUSD",
- *         "decimals": 18,
- *         "addresses": {
- *             "56": "0xe9e7cea3dedca5984780bafc599bd69add087d56"
- *         },
- *         "swapType": "USD"
+ *       "error": "A valid value for chain must be passed"
  *     }
- *     ...
- * ]
  *
  * @apiSampleRequest /v1/get_bridgeable_tokens
  */
@@ -73,18 +82,27 @@ router.get('/get_bridgeable_tokens',
  * @apiName get_chains_for_token
  * @apiGroup API
  *
- * @apiQuery {String} token Token address on chain
+ * @apiQuery {String} token Token symbol or chain address
+ *
+ * @apiExample {curl} Example usage:
+ * curl --request GET 'https://syn-api-x.herokuapp.com/v1/get_chains_for_token?token=DAI'
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
- * [
+ *     [
+ *         {
+ *             "name": "Ethereum Mainnet",
+ *             "chainId": 1,
+ *             "chainCurrency": "ETH"
+ *         },
+ *         ...
+ *     ]
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
  *     {
- *         "name": "Ethereum Mainnet",
- *         "chainId": 1,
- *         "chainCurrency": "ETH"
- *     },
- *     ...
- * ]
+ *       "error": "A valid token must be passed"
+ *     }
  *
  * @apiSampleRequest /v1/get_chains_for_token
  */
@@ -121,12 +139,21 @@ router.get('/get_chains_for_token',
  * @apiQuery {String} toToken Token user will receive from the bridge on the destination chain
  * @apiQuery {String} amountFrom Transaction input amount
  *
+ * @apiExample {curl} Example usage:
+ *      curl --request GET 'http://syn-api-x.herokuapp.com/v1/estimate_bridge_output?fromChain=AVALANCHE&toChain=BSC&fromToken=USDC&toToken=USDC&amountFrom=10'
+ *
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
- * {
- *     "amountToReceive": "0",
- *     "bridgeFee": "2000000000000000000"
- * }
+ *      {
+ *          "amountToReceive": "0",
+ *          "bridgeFee": "2000000000000000000"
+ *      }
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Valid arguments for fromChain, toChain, fromToken, toToken and amountFrom must be passed"
+ *     }
  *
  * @apiSampleRequest /v1/estimate_bridge_output
  */
@@ -135,7 +162,7 @@ router.get('/estimate_bridge_output',
     oneOf([check('toChain').isIn(ChainUtils.getNames()), check('toChain').isIn(ChainUtils.getIds()), check('toChain').isIn(ChainUtils.getHexIds())]),
     oneOf([check('fromToken').isIn(TokenUtils.getSymbols()), check('fromToken').isIn(TokenUtils.getAddresses())]),
     oneOf([check('toToken').isIn(TokenUtils.getSymbols()), check('toToken').isIn(TokenUtils.getAddresses())]),
-    check('amountFrom').exists(),
+    check('amountFrom').isNumeric(),
     async (req, res) => {
 
         try {
@@ -151,8 +178,12 @@ router.get('/estimate_bridge_output',
             const estimate = await estimateBridgeOutputs(fromChain, toChain, fromToken, toToken, amountFrom);
             res.status(200).json(estimate);
         } catch (err) {
-            console.log(err);
-            res.status(500).json({"error": "Internal Server Error"});
+
+            if (err.message.includes("not supported on")) {
+                res.status(400).json({"error": `${err.message}`});
+            } else {
+                res.status(500).json({"error": "Internal Server Error"});
+            }
         }
     });
 
@@ -168,14 +199,23 @@ router.get('/estimate_bridge_output',
  * @apiQuery {Number} amountFrom Amount that the user will send to the bridge on the source chain
  * @apiQuery {String} address Optional, user can provide an address other than the one retrieved from signer to receive tokens
  *
+ * @apiExample {curl} Example usage:
+ *      curl --request GET 'https://syn-api-x.herokuapp.com/v1/generate_unsigned_bridge_txn?fromChain=AVALANCHE&toChain=BSC&fromToken=USDC&toToken=USDC&amountFrom=10&address=0x2D2c027E0d1A899a1965910Dd272bcaE1cD03c22'
+ *
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
- * {
- *     "unsigned_data": "0x9f33072700000000000000...",
- *     "to": "0xE85429C97589AD793Ca11A8BC3477C03d27ED140",
- *     "gasPrice": "150000000000",
- *     "gasLimit": "150000000000"
- * }
+ *      {
+ *          "unsigned_data": "0x9f33072700000000000000...",
+ *          "to": "0xE85429C97589AD793Ca11A8BC3477C03d27ED140",
+ *          "gasPrice": "150000000000",
+ *          "gasLimit": "150000000000"
+ *      }
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Valid arguments for fromChain, toChain, fromToken, toToken, amountFrom and address must be passed"
+ *     }
  *
  * @apiSampleRequest /v1/generate_unsigned_bridge_txn
  */
@@ -214,16 +254,25 @@ router.get('/generate_unsigned_bridge_txn',
  * @apiQuery {Number|String} fromChain Name or decimal/hex id of chain
  * @apiQuery {String} fromToken Token instance or valid on-chain address of the token the user will be sending to the bridge on the source chain
  *
+ * @apiExample {curl} Example usage:
+ *      curl --request GET 'https://syn-api-x.herokuapp.com/v1/generate_unsigned_bridge_approval_txn?fromChain=ETH&fromToken=ETH'
+ *
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
- * {
- *     "unsigned_data": "0x095ea7b3000000000000000000000000...",
- *     "to": "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
- *     "maxPriorityFeePerGas": {
- *         "type": "BigNumber",
- *         "hex": "0x59682f00"
+ *      {
+ *          "unsigned_data": "0x095ea7b3000000000000000000000000...",
+ *          "to": "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
+ *          "maxPriorityFeePerGas": {
+ *              "type": "BigNumber",
+ *              "hex": "0x59682f00"
+ *          }
+ *      }
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Valid arguments for fromChain and fromToken must be passed"
  *     }
- * }
  *
  * @apiSampleRequest /v1/generate_unsigned_bridge_approval_txn
  */
@@ -260,49 +309,58 @@ router.get('/generate_unsigned_bridge_approval_txn',
  * @apiQuery {Number} amountFrom Amount of tokenFrom (denoted in wei) that the user will send to the bridge on the source chain
  * @apiQuery {String} address Optional, user can provide an address other than the one retrieved from signer to receive tokens
  *
+ * @apiExample {curl} Example usage:
+ *      curl --request GET 'https://syn-api-x.herokuapp.com/v1/generate_bridge_txn_params?fromChain=1&toChain=BSC&fromToken=USDC&toToken=USDC&amountFrom=1&amountTo=1'
+ *
  * @apiSuccessExample Success-Response:
  *      HTTP/1.1 200 OK
- * {
- *     "tokenFrom": {
- *         "name": "USD Circle",
- *         "symbol": "USDC",
- *         "addresses": {
- *             "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
- *             ...
- *         },
- *         "swapType": "USD",
- *         "isETH": false,
- *         "wrapperAddresses": {},
- *         "_decimals": {
- *             "1": 6,
- *             ...
- *         }
- *     },
- *     "tokenTo": {
- *         "name": "USD Circle",
- *         "symbol": "USDC",
- *         "addresses": {
- *             "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
- *             ...
- *         },
- *         "swapType": "USD",
- *         "isETH": false,
- *         "wrapperAddresses": {},
- *         "_decimals": {
- *             "1": 6,
- *             ...
- *         }
- *     },
- *     "chainIdTo": 56,
- *     "amountFrom": {
- *         "type": "BigNumber",
- *         "hex": "0x01"
- *     },
- *     "amountTo": {
- *         "type": "BigNumber",
- *         "hex": "0x01"
- *     }
+ *      {
+ *          "tokenFrom": {
+ *              "name": "USD Circle",
+ *              "symbol": "USDC",
+ *              "addresses": {
+ *                  "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+ *                  ...
+ *              },
+ *              "swapType": "USD",
+ *              "isETH": false,
+ *              "wrapperAddresses": {},
+ *              "_decimals": {
+ *                  "1": 6,
+ *                  ...
+ *              }
+ *          },
+ *          "tokenTo": {
+ *              "name": "USD Circle",
+ *              "symbol": "USDC",
+ *              "addresses": {
+ *                  "1": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+ *                  ...
+ *              },
+ *              "swapType": "USD",
+ *              "isETH": false,
+ *              "wrapperAddresses": {},
+ *              "_decimals": {
+ *                  "1": 6,
+ *                  ...
+ *              }
+ *          },
+ *          "chainIdTo": 56,
+ *          "amountFrom": {
+ *              "type": "BigNumber",
+ *              "hex": "0x01"
+ *          },
+ *          "amountTo": {
+ *              "type": "BigNumber",
+ *              "hex": "0x01"
+ *          }
  * }
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Valid arguments for fromChain, toChain, fromToken, toToken, amountFrom and amountTo must be passed"
+ *     }
  *
  * @apiSampleRequest /v1/generate_bridge_txn_params
  */
@@ -340,6 +398,48 @@ router.get('/generate_bridge_txn_params',
  * @apiGroup API
  *
  * @apiQuery {Number|String} chain Name or decimal/hex id of chain
+ *
+ *     {
+ *         "nUSD": {
+ *             "baseToken": {
+ *                 "name": "Synapse nUSD LP Token Ethereum",
+ *                 "symbol": "nUSD",
+ *                 "addresses": {
+ *                     "1": "0x1B84765dE8B7566e4cEAF4D0fD3c5aF52D3DdE4F"
+ *                 },
+ *                 "swapType": "USD",
+ *                 "isETH": false,
+ *                 "wrapperAddresses": {},
+ *                 "_decimals": {
+ *                     "1": 18
+ *                 }
+ *             },
+ *             "poolId": 420,
+ *             "poolName": "Ethereum Stableswap Pool ",
+ *             "poolType": "USD",
+ *             "poolTokens": [
+ *                 {
+ *                     "name": "Dai",
+ *                     "symbol": "DAI",
+ *                     "addresses": {
+ *                         "1": "0x6b175474e89094c44da98b954eedeac495271d0f",
+ *                         "56": "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3",
+ *                         ...
+ *                      }
+ *                  },
+ *                  ...
+ *             ]
+ *         },
+ *         "nETH": {
+ *             ...
+ *         }
+ *     }
+ *
+ * @apiErrorExample {json} Error - Invalid Arguments:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "Valid arguments for chain must be passed"
+ *     }
  *
  * @apiSampleRequest /v1/get_stableswap_pools
  */
