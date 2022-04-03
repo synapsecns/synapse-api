86599a72e48029abada215335dfde4c24ade0043
bholagabbar <dev.shreyans96@gmail.com>
2022-04-03 09:35:18 -0400

    Implement token swap map

---
+++ server/controllers/getSwapTokenMap.js
@@ -0,0 +1,25 @@
+import { networkSwapTokensMap } from "@synapseprotocol/sdk";
+import * as ChainUtils from "../utils/chainUtils.js";
+import {removeDecimalUnderscoreFromJSON} from "../utils/responseUtils.js"
+
+/**
+ * @param {String} chainA
+ * @param {String} chainB
+ * @returns {Object[]}
+ */
+async function getSwapTokenMap(chainA, chainB) {
+    try {
+        const chainIdA = ChainUtils.getIdFromRequestQueryParam(chainA);
+        const chainIdB = ChainUtils.getIdFromRequestQueryParam(chainB);
+
+        let res = networkSwapTokensMap(chainIdA, chainIdB);
+        removeDecimalUnderscoreFromJSON(res)
+
+        return res[chainIdB]
+    } catch (err) {
+        console.log(err);
+        throw err
+    }
+}
+
+export { getSwapTokenMap };
